8d674595bb41589f69469599cbdd3e1828e9b560
chnicolasdev <christian.cnod@gmail.com>
2023-07-23 22:19:16 -0400

    Agrega estados para el TodoCounter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TodoCounter } from './components/TodoCounter/TodoCounter';
 import { TodoSearch } from './components/TodoSearch/TodoSearch';
 import { TodoList } from './components/TodoList/TodoList';
@@ -5,21 +6,38 @@ import { TodoItem } from './components/TodoItem/TodoItem';
 import { CreateTodoButton } from './components/CreateTodoButton/CreateTodoButton';
 
 const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Hacer aseo', completed: false },
-  { text: 'Limpiar ventana', completed: false },
+  { text: 'Cortar cebolla',     completed: true },
+  { text: 'Hacer aseo',         completed: true },
+  { text: 'Limpiar ventana',    completed: false },
   { text: 'Ir al supermercado', completed: false }
 ];
 
 function App() {
+
+  // Estado input search
+  const [searchValue, setSearchValue] = React.useState('');
+  
+  // Estado Listas Todo
+  const [todos, setTodos] = React.useState(defaultTodos);
+  
+  // Se recorren los todos (del defaultTodos) y si tiene completed true, se retorna.
+  const completedTodos  = todos.filter(
+                                  todo => !!todo.completed // Por cada completed, se agrega al array, la doble negación, convierte en boleano
+                          ).length; // Se ve el tamaño del array
+
+  // Se cuentan todos los todos que se crearon arriba.
+  const totalTodos = todos.length; 
+  
+  // Input searchValue
+  console.log(`Buscar: ${searchValue}`);
+
   return (
     <>
-      <TodoCounter
-        completed={16}
-        total={25}
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
       />
-      
-      <TodoSearch />
 
       <TodoList>
         {defaultTodos.map( todo => (
--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -2,16 +2,7 @@ import React from 'react';
 import './TodoSearch.css';
 import searchIcon from '../../assets/search-icon.svg';
 
-function TodoSearch(props) {
-
-  // Estado
-  const [searchValue, setSearchValue] = React.useState('');
-
-  // Función para capturar los eventos
-  const searchTodo = (e) => {
-    setSearchValue(e.target.value);
-    console.log(`Los usuarios buscan: ${e.target.value}`);
-  }
+function TodoSearch({ searchValue, setSearchValue }) {
 
   return (
     <div className="search-container">
@@ -19,7 +10,7 @@ function TodoSearch(props) {
           placeholder="Cortar cebolla" 
           value={searchValue}
           onChange={
-            (event) => searchTodo(event)
+            (event) => setSearchValue(event.target.value)
           }
         />
         <img className="search-icon" src={searchIcon} alt="search icon" />
